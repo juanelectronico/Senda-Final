1933967c306d1ac08b9c9085e39d437352c93ae5
juan carlos viera <juancarlos.viera@gmail.com>
2026-08-15 09:47:37 -0600

    Activando bot de WhatsApp con Gemini

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Inicia el servidor web en segundo plano
 node server-baileys.cjs &
-
-# Inicia el bot de WhatsApp en segundo plano
 node dist/index.js &
-
-# Mantén el contenedor abierto para siempre
-tail -f /dev/null
+wait
